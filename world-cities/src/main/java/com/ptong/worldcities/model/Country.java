bcd8bad245916e302e6e0a7233a017a7185eacc5
Philip Tong <philiptong@users.noreply.github.com>
2020-08-02 10:20:44 +0100

    Update models

--- world-cities/src/main/java/com/ptong/worldcities/model/Country.java
+++ world-cities/src/main/java/com/ptong/worldcities/model/Country.java
@@ -5,16 +5,11 @@ import java.io.Serializable;
 import lombok.Data;
 
 @Data
-public class City implements Serializable {
+public class Country implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     private String name;
-    private double lat;
-    private double lng;
-    private String countryName;
     private String isoCode2;
     private String isoCode3;
-    private String adminName;
-
 }
