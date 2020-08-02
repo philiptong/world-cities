764734a325f2ade77d1bf48fc97749f9f699d2c7
Philip Tong <philiptong@users.noreply.github.com>
2020-08-02 10:20:27 +0100

    Add constants

---
+++ world-cities/src/main/java/com/ptong/worldcities/config/Constants.java
@@ -0,0 +1,5 @@
+package com.ptong.worldcities.config;
+
+public class Constants {
+    public static final String CHINA_CITIES_RESOURCE_URI = "http://www.mca.gov.cn///article/sj/xzqh/2020/2020/2020072805001.html";
+}
--- world-cities/src/test/java/com/ptong/worldcities/collector/ChinaCitiesCollectorTests.java
+++ world-cities/src/test/java/com/ptong/worldcities/collector/ChinaCitiesCollectorTests.java
@@ -3,6 +3,7 @@ package com.ptong.worldcities.collector;
 import java.io.IOException;
 import java.util.List;
 
+import com.ptong.worldcities.config.Constants;
 import com.ptong.worldcities.model.City;
 
 import org.junit.jupiter.api.Assertions;
@@ -10,12 +11,12 @@ import org.junit.jupiter.api.Test;
 
 public class ChinaCitiesCollectorTests {
 
-    private final String RESOURCE_URL = "http://www.mca.gov.cn///article/sj/xzqh/2020/2020/2020072805001.html";
+    private final String RESOURCE_URI = Constants.CHINA_CITIES_RESOURCE_URI;
 
     @Test
     public void shouldDownloadHtml() throws IOException, InterruptedException {
         ChinaCitiesCollector collector = new ChinaCitiesCollector();
-        String html = collector.downloadHtml(this.RESOURCE_URL);
+        String html = collector.downloadHtml(this.RESOURCE_URI);
         System.out.println(html);
         Assertions.assertTrue(html.length() > 0);
     }
@@ -23,7 +24,7 @@ public class ChinaCitiesCollectorTests {
     @Test
     public void shouldParseHtml() throws IOException, InterruptedException {
         ChinaCitiesCollector collector = new ChinaCitiesCollector();
-        String html = collector.downloadHtml(this.RESOURCE_URL);
+        String html = collector.downloadHtml(this.RESOURCE_URI);
         List<City> cities = collector.parseHtml(html);
         System.out.println(cities);
         Assertions.assertTrue(cities.size() > 0);
