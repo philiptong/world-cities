f939d730bc74accdb19909858ed634a36b662160
Philip Tong <philiptong@users.noreply.github.com>
2020-08-03 15:24:48 +0100

    Rename and add Malaysia

--- world-cities/src/main/java/com/ptong/worldcities/model/Region.java
+++ world-cities/src/main/java/com/ptong/worldcities/model/Region.java
@@ -16,6 +16,6 @@ public class Region implements Serializable {
     private String name;
     private String isoCode;
     private String countryCode;
-    private List<District> cities;
+    private List<District> districts;
 
 }
---
+++ world-cities/src/test/java/com/ptong/worldcities/collector/MYSRegionCollectorTests.java
@@ -0,0 +1,47 @@
+package com.ptong.worldcities.collector;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.ptong.worldcities.collector.region.MYSRegionCollector;
+import com.ptong.worldcities.model.District;
+import com.ptong.worldcities.model.Region;
+import com.ptong.worldcities.utils.FileUtils;
+import com.ptong.worldcities.utils.JsonUtils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MYSRegionCollectorTests {
+
+    private final String REGION_HTML_FILE_PATH = FileUtils.getTestResourcesFolder() + "/mys/regions_html";
+    private final String DISTRICT_JSON_FILE_PATH = FileUtils.getTestResourcesFolder() + "/mys/districts_json";
+    private final String REGION_JSON_FILE_PATH = FileUtils.getTestResourcesFolder() + "/mys/regions_json";
+
+    @Test
+    public void shouldParseDistrictHtml() throws IOException, InterruptedException {
+        MYSRegionCollector collector = new MYSRegionCollector();
+        String html = FileUtils.readFile(REGION_HTML_FILE_PATH);
+        List<District> districts = collector.parseDistrictHtml(html);
+        FileUtils.writeToFile(DISTRICT_JSON_FILE_PATH, JsonUtils.getJsonFromObject(districts));
+        Assertions.assertTrue(districts.size() == 16);
+    }
+
+    @Test
+    public void shouldDownloadRegionHtml() throws IOException, InterruptedException {
+        MYSRegionCollector collector = new MYSRegionCollector();
+        String html = collector.downloadRegionHtml();
+        FileUtils.writeToFile(REGION_HTML_FILE_PATH, html);
+        Assertions.assertTrue(html.length() > 0);
+    }
+
+    @Test
+    public void shouldParseRegionHtml() throws IOException, InterruptedException {
+        MYSRegionCollector collector = new MYSRegionCollector();
+        String html = FileUtils.readFile(REGION_HTML_FILE_PATH);
+        List<Region> regions = collector.parseRegionHtml(html);
+        FileUtils.writeToFile(REGION_JSON_FILE_PATH, JsonUtils.getJsonFromObject(regions));
+        Assertions.assertTrue(regions.size() == 16);
+    }
+
+}
